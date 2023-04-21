0f0464804edbeaaa75082bffdceacdcdfc2b3cc8
Cazuza Neto <cazuzant@gmail.com>
2023-04-21 11:13:38 -0300

    feat: add unit test into bank slip repository

--- src/db/bankSlipRepository.js
+++ src/db/bankSlipRepository.js
@@ -1,7 +1,6 @@
 const mysql = require('mysql');
 require('dotenv').config();
 
-// Função que faz a conexão com o banco de dados
 async function init() {
   const connection = await mysql.createConnection({
     host: process.env.DB_HOST,
@@ -12,7 +11,6 @@ async function init() {
   return connection;
 }
 
-// Função que consulta um registro por id
 async function getOne(id) {
   const connection = await init();
   const [rows] = await connection.query('SELECT * FROM bankslips WHERE id = ?', [id]);
@@ -23,7 +21,6 @@ async function getOne(id) {
   return rows[0];
 }
 
-// Função que lista todos os registros
 async function getAll() {
   const connection = await init();
   const [rows] = await connection.query('SELECT * FROM bankslips');
@@ -31,7 +28,6 @@ async function getAll() {
   return rows;
 }
 
-// Função que remove um registro por id
 async function remove(id) {
   const connection = await init();
   const [result] = await connection.query('DELETE FROM bankslips WHERE id = ?', [id]);
@@ -39,20 +35,18 @@ async function remove(id) {
   return result.affectedRows > 0;
 }
 
-// Função que atualiza um registro
-async function update(entidade) {
-  const { id } = entidade;
-  delete entidade.id;
+async function update(entity) {
+  const { id } = entity;
+  delete entity.id;
   const connection = await init();
-  const [result] = await connection.query('UPDATE bankslips SET ? WHERE id = ?', [entidade, id]);
+  const [result] = await connection.query('UPDATE bankslips SET ? WHERE id = ?', [entity, id]);
   connection.end();
   return result.affectedRows > 0;
 }
 
-// Função que cria um novo registro
-async function create(entidade) {
+async function create(entity) {
   const connection = await init();
-  const [result] = await connection.query('INSERT INTO bankslips SET ?', [entidade]);
+  const [result] = await connection.query('INSERT INTO bankslips SET ?', [entity]);
   connection.end();
   return result.insertId;
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 
-const UserController = require('./controllers/Usercontroller')
+const UserController = require('../controllers/Usercontroller')
 
 router.post('/login', UserController.login)
 
