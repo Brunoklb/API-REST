d5d4defdd65c087dd1fe12c54d0806d27693c74b
Cazuza Neto <cazuzant@gmail.com>
2023-04-21 18:20:57 -0300

    feat: creates bank slips controller

---
+++ src/controllers/BankSlipController.js
@@ -0,0 +1,26 @@
+const { getOne, getAll } = require('./database');
+
+async function getBankSlipById(req, res) {
+    const { id } = req.params;
+
+    try {
+        const bankSlip = await getOne(id);
+        if (!bankSlip) {
+            return res.status(404).json({ message: 'Bank slip not found' });
+        }
+        res.json(bankSlip);
+    } catch (err) {
+        res.status(500).json({ message: 'Error retrieving bank slip' });
+    }
+}
+
+async function getAllBankSlips(req, res) {
+    try {
+        const bankSlips = await getAll();
+        res.json(bankSlips);
+    } catch (err) {
+        res.status(500).json({ message: 'Error retrieving bank slips' });
+    }
+}
+
+module.exports = { getAllBankSlips, getBankSlipById };
