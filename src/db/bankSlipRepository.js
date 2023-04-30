cb088bf6120bfeece6b13022c6db83ff8e085a61
Brunoklb <brunola.nogueira@gmail.com>
2023-04-30 16:15:52 -0300

    Changing the test code

--- src/db/bankSlipRepository.js
+++ src/db/bankSlipRepository.js
@@ -1,41 +1,43 @@
 const mysql = require('mysql');
 require('dotenv').config();
 
-async function init() {
+async function init(){
   const connection = await mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    database: process.env.DB_NAME
   });
   return connection;
 }
 
-async function getOne(id) {
+async function getOne(id){
   const connection = await init();
   const [rows] = await connection.query('SELECT * FROM bankslips WHERE id = ?', [id]);
   connection.end();
-  if (rows.length === 0) {
+  if(rows.length === 0){
     return null;
   }
-  return rows[0];
+  else{
+    return rows[0];
+  }
 }
 
-async function getAll() {
+async function getAll(){
   const connection = await init();
   const [rows] = await connection.query('SELECT * FROM bankslips');
   connection.end();
   return rows;
 }
 
-async function remove(id) {
+async function remove(id){
   const connection = await init();
   const [result] = await connection.query('DELETE FROM bankslips WHERE id = ?', [id]);
   connection.end();
   return result.affectedRows > 0;
 }
 
-async function update(entity) {
+async function update(entity){
   const { id } = entity;
   delete entity.id;
   const connection = await init();
@@ -44,7 +46,7 @@ async function update(entity) {
   return result.affectedRows > 0;
 }
 
-async function create(entity) {
+async function create(entity){
   const connection = await init();
   const [result] = await connection.query('INSERT INTO bankslips SET ?', [entity]);
   connection.end();
